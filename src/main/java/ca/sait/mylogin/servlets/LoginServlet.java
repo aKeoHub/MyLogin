428fe99955b71f263fb128d202759c269dbff749
Andy Keobounphan <andykeo@live.ca>
2022-02-03 14:47:40 -0700

    Generated logout functionality.
    When user logsout a message displays and brings them back to the home page. User can not try to return to the home page without pressing the logout button.

--- src/main/java/ca/sait/mylogin/servlets/LoginServlet.java
+++ src/main/java/ca/sait/mylogin/servlets/LoginServlet.java
@@ -1,4 +1,3 @@
-
 package ca.sait.mylogin.servlets;
 
 import ca.sait.mylogin.models.User;
@@ -9,15 +8,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author Kingston
  */
 public class LoginServlet extends HttpServlet {
-
-
-
 
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -30,13 +27,22 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-        String query = request.getQueryString();
-        
-        if (query != null && query.contains("logout")) {
-            
+
+        HttpSession session = request.getSession();
+
+        if (session.getAttribute("username") != null) {
+
+            String query = request.getQueryString();
+
+            if (query != null && query.contains("logout")) {
+                session.invalidate();
+                request.setAttribute("message", "You are logged out");
+            } else {
+                response.sendRedirect("home");
+                return;
+            }
         }
-        
+
         getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
     }
 
@@ -51,33 +57,30 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        
+
         if (username == null || username.isEmpty() || password == null || password.isEmpty()) {
             request.setAttribute("message", "Username or password is missing.");
         } else {
             AccountService account = new AccountService();
-            
+
             User user = account.login(username, password);
-            if (user != null ) {
+            if (user != null) {
                 request.getSession().setAttribute("username", username);
-                
+
                 response.sendRedirect("home");
                 return;
-                
+
             } else {
-           request.setAttribute("username", username);
-           request.setAttribute("message", "Username or password was incorrect");
-           
-           
+                request.setAttribute("username", username);
+                request.setAttribute("message", "Username or password was incorrect");
+
             }
         }
-        
-        
+
         getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
     }
-
 
 }
